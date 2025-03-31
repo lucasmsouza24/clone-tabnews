1ca373525f578a1a6af93e1c90338129cb2ea46c
lucasmsouza24 <lucasmesquita58@hotmail.com>
2025-03-31 19:44:15 -0300

    feat: :speech_balloon: Modifying 'Under Construction' page phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-  return <h1>Hello Gab!</h1>;
+  return <>
+    <h1>🚧 Em Construção 🚧</h1>
+    <a href="https://github.com/lucasmsouza24" target="_blank">Github</a>
+  </> 
 }
 
 export default Home;
