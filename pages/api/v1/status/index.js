d89c4412ce6276490059a9b7d196c368b1f07411
lucasmsouza24 <lucasmesquita58@hotmail.com>
2025-04-01 18:44:27 -0300

    feat: :sparkles: Creating database connection and adding credentials to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const res = await database.query('SELECT 1 + 1 as sum;')
+  console.log(res.rows[0])
+
   response.status(200).json({"message": "endpoint /api/v1/status working!"})
 }
 
