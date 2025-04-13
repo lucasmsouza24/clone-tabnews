ceb8bd1836ff355328f5a9083d37d4f5ead63406
lucasmsouza24 <lucasmesquita58@hotmail.com>
2025-04-13 00:53:59 -0300

    refactor: :lipstick: Adding `...` to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return <>
-    <h1>🚧 Em Construção 🚧</h1>
+    <h1>🚧 Em Construção... 🚧</h1>
     <a href="https://github.com/lucasmsouza24" target="_blank">Github</a>
   </> 
 }
